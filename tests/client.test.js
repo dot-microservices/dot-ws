0028f542bb38831c2518433caae5dc82f666f711
umu+ <aydinumut82@gmail.com>
2019-05-09 11:31:34 +0300

    test case implemented

---
+++ tests/client.test.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+'use strict';
+
+'use strict';
+
+const Client = require('../src/client');
+const Server = require('../src/server');
+
+class TestService {
+    static _name() {
+        return 't';
+    }
+
+    static echo(request, reply) {
+        reply(request);
+    }
+}
+
+const server = new Server({});
+server.start();
+const client = new Client({});
+server.addService(TestService);
+
+test('service health', async done => {
+    const now = Date.now();
+    setTimeout(() => {
+        client.send('t.echo', { now }, r => {
+            expect(r.now).toBe(now);
+            client.disconnect();
+            server.shutdown();
+            done();
+        });
+    }, 1200);
+});
